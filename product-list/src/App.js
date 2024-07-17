5ddeebc01b5101a5a765c407a579b51d8bf36f36
Gitax18 <lusifer0418@gmail.com>
2024-07-17 18:23:50 +0530

    made product list responsive

--- product-list/src/App.js
+++ product-list/src/App.js
@@ -1,17 +1,23 @@
 import data from "./data.json";
 import { useState } from "react";
 
-function Card({ dt }) {
+function Card({ dt, setCart }) {
   const [quantity, setQuantity] = useState(null);
+
+  function handleCartDetail() {
+    const item = { ...dt, quantity: quantity };
+  }
+
+  function handleEmptyCart() {
+    setQuantity(1);
+  }
+
   return (
     <div className="card">
       <div className="img-container">
         <img src={dt.image.desktop} alt={dt.name} />
         {quantity === null ? (
-          <button
-            className="cart-btn btn-cart-add"
-            onClick={(e) => setQuantity(1)}
-          >
+          <button className="cart-btn btn-cart-add" onClick={handleEmptyCart}>
             <img src="./assets/images/icon-add-to-cart.svg" />
             Add to Cart
           </button>
@@ -36,22 +42,26 @@ function Card({ dt }) {
   );
 }
 
-function Cards({ data }) {
+function Cards({ data, setCart }) {
   return (
     <div className="cards">
       {data.map((dt) => (
-        <Card dt={dt} />
+        <Card dt={dt} setCart={setCart} key={crypto.randomUUID()} />
       ))}
     </div>
   );
 }
 
+function Cart() {}
+
 export default function App() {
+  const [cart, setCart] = useState([]);
+  console.log(cart);
   return (
     <div className="container">
       <h1 className="heading">Desserts</h1>
       <main>
-        <Cards data={data} />
+        <Cards data={data} setCart={setCart} />
       </main>
     </div>
   );
